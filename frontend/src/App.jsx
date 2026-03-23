d1ddbf303548e0f6d6260be2c7eb895b281b2af2
Amit jain <ajain@zuelligpharma.com>
2026-03-23 10:41:15 +0530

    drive me latest cors update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -178,7 +178,7 @@ function App() {
             }
           />
           {/* <Route
-            path="/my-quotations"
+            path="/my-quotation"
             element={
               <ProtectedRoute allowedRoles={["CORPORATE"]}>
                 <MyQuotations />
