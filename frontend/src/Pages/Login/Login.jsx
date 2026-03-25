89d89c135a666b908fe7962a5009404dddc705ad
Amit jain <ajain@zuelligpharma.com>
2026-03-25 13:22:29 +0530

    latest

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -174,8 +174,8 @@ const Login = () => {
                   {showPassword ? (
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
+                      width="17"
+                      height="17"
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
@@ -189,8 +189,8 @@ const Login = () => {
                   ) : (
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
+                      width="17"
+                      height="17"
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
--- frontend/src/Pages/Register/Register.jsx
+++ frontend/src/Pages/Register/Register.jsx
@@ -737,8 +737,8 @@ const Register = () => {
                       {showPassword ? (
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          width="20"
-                          height="20"
+                          width="17"
+                          height="17"
                           viewBox="0 0 24 24"
                           fill="none"
                           stroke="currentColor"
@@ -752,8 +752,8 @@ const Register = () => {
                       ) : (
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          width="20"
-                          height="20"
+                          width="17"
+                          height="17"
                           viewBox="0 0 24 24"
                           fill="none"
                           stroke="currentColor"
